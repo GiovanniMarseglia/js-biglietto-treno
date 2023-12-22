23725dc3e786168816419d055749546a861f9506
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2023-12-22 16:48:42 +0100

    added element in html for the results

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-let age = prompt( "inserire la propria età" );
-let km = prompt( "inserire il numero di km da percorrere" );
+let loginForm = document.getElementById("inv");
+
+
+loginForm.addEventListener("click", function() {
+
+let age = document.getElementById("age1").value;
+let km = document.getElementById("km2").value;
 let total_price;
 const price = 0.21;
 let first_price = km * price;
@@ -7,20 +12,23 @@ const under = 20;
 const over = 40;
 
 if(age == "" || km == ""){
-    document.writeln("I campi non possono rimanere vuoti10");
+    document.writeln("I campi non possono rimanere vuoti");
 }
 else{
 if (age < 18){
     total_price = first_price - ((first_price / 100) * under);
-    document.writeln(`<p>Il prezzo del biglietto è di:${first_price.toFixed(2)}€, in quanto minorenne per lei è riserveto uno sconto del 20%. </P>`); 
-    document.writeln(`<p>Il prezzo finale dovuto è di:${total_price.toFixed(2)}€ </P>`);
+    document.getElementById("prz").innerHTML = (`<p>Il prezzo del biglietto è di: ${first_price.toFixed(2)}€, in quanto minorenne per lei è riserveto uno sconto del 20%. </P>`); 
+    document.getElementById("sco").innerHTML = (`<p>Il prezzo finale con lo sconto applicato è di: ${total_price.toFixed(2)}€ </P>`);
+
+    
 }
 else if (age > 65){
     total_price = first_price - ((first_price / 100) * over);
-    document.writeln(`<p>Il prezzo del biglietto è di:${first_price.toFixed(2)}€, in quanto maggiorenne per lei è riserveto uno sconto del 40%. </P>`); 
-    document.writeln(`<p>Il prezzo finale dovuto è di:${total_price.toFixed(2)}€ </P>`);
+    document.getElementById("prz").innerHTML = (`<p>Il prezzo del biglietto è di:${first_price.toFixed(2)}€, in quanto over 65 per lei è riserveto uno sconto del 40%. </P>`); 
+    document.getElementById("sco").innerHTML = (`<p>Il prezzo finale con lo sconto applicato è di: ${total_price.toFixed(2)}€ </P>`);
 }
 else{
-    document.writeln(`<p>Il prezzo totale del biglietto è di: ${first_price.toFixed(2)}€ </P>`);
+    document.getElementById("prz").innerHTML = (`<p>Il prezzo totale del biglietto è di: ${first_price.toFixed(2)}€ </P>`);
 }
 }
+});
